55ffe744c7f43253738b9c56b4da78291023735d
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2022-01-06 15:01:04 +0700

    Init devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root "home#index"
+
+  devise_for :admin_users, controllers: {
+    registrations: "admin_users/registrations",
+    sessions: "admin_users/sessions"
+  }
 end
